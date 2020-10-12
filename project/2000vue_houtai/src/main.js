53395f382a09fc611c2aa0dccd90fd15796a567e
周玉森 <2871706122@qq.com>
2020-10-12 20:06:05 +0800

    引入echarts和element_ui

--- project/2000vue_houtai/src/main.js
+++ project/2000vue_houtai/src/main.js
@@ -3,6 +3,18 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+var echarts = require('echarts');
+Vue.prototype.$echarts = echarts
+
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+import locale from 'element-ui/lib/locale/lang/zh-CN'
+
+Vue.use(ElementUI, { locale })
+
+Vue.use(ElementUI);
+
 Vue.config.productionTip = false
 
 new Vue({
--- project/2000vue_houtai/src/views/Home.vue
+++ project/2000vue_houtai/src/views/Home.vue
@@ -1,7 +1,21 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-select v-model="value" placeholder="请选择">
+      <el-option
+          v-for="item in options"
+          :key="item.value"
+          :label="item.label"
+          :value="item.value">
+      </el-option>
+    </el-select>
+
+    <el-date-picker
+        v-model="value1"
+        type="date"
+        placeholder="选择日期">
+    </el-date-picker>
+
+    <div ref="dom" style="width: 500px;height: 400px;"></div>
   </div>
 </template>
 
@@ -13,6 +27,48 @@ export default {
   name: 'Home',
   components: {
     HelloWorld
+  },
+  data(){
+    return {
+      value1: '',
+      options: [{
+        value: '选项1',
+        label: '黄金糕'
+      }, {
+        value: '选项2',
+        label: '双皮奶'
+      }, {
+        value: '选项3',
+        label: '蚵仔煎'
+      }, {
+        value: '选项4',
+        label: '龙须面'
+      }, {
+        value: '选项5',
+        label: '北京烤鸭'
+      }],
+      value: ''
+    }
+  },
+  mounted() {
+    // 基于准备好的dom，初始化echarts实例
+    let myChart = this.$echarts.init(this.$refs.dom);
+    // 绘制图表
+    myChart.setOption({
+      title: {
+        text: 'ECharts 入门示例'
+      },
+      tooltip: {},
+      xAxis: {
+        data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+      },
+      yAxis: {},
+      series: [{
+        name: '销量',
+        type: 'bar',
+        data: [5, 20, 36, 10, 10, 20]
+      }]
+    });
   }
 }
 </script>
